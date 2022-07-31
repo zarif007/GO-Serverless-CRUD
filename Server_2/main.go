b3f03180edf6791894ea3db021ebe6076fb21de4
zarif007 <zarifhuq786@gmail.com>
2022-08-01 01:42:48 +0600

    Added api gateaway

--- Server_2/main.go
+++ Server_2/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,11 +13,40 @@ func main() {
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Println("HGello")
+
+	var person Person
+
+	err := json.Unmarshal([]byte(request.Body), &person)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	msg := fmt.Sprintf("Hello %v %v", *person.FirstName, *person.LastName)
+
+	responseBody := ResponseBody{
+		Message: &msg,
+	}
+
+	jbytes, err := json.Marshal(responseBody)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
 
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
+		Body:       string(jbytes),
 	}
 
 	return response, nil
+}
+
+type Person struct {
+	FirstName *string `json:"firstname"`
+	LastName  *string `json:"lastname"`
+}
+
+type ResponseBody struct {
+	Message *string `json:"message"`
 }
