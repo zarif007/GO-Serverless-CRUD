1704b7eedf7b86417da97027aebdfb4a64a3bd09
zarif007 <zarifhuq786@gmail.com>
2022-08-09 02:51:11 +0600

    Adding server_4

---
+++ Server_4/cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+
+	"github.com/aws-sdk-go/aws"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/zarif007/GO-Serverless-CRUD/Server_4/handlers"
+)
+
+var (
+	dynaClient dynamodbiface.DynamoDBAPI
+)
+
+func main() {
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)})
+
+	if err != nil {
+		return
+	}
+
+	dynaClient = dynamodb.New(awsSession)
+	lambda.Start(handler)
+}
+
+const tableName = "LambdaInGoUser"
+
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse) {
+	switch req.HTTPMethod {
+	case "GET":
+		return handlers.GetUser(req, tableName, dynaClient)
+	case "POST":
+		return handlers.CreateUser(req, tableName, dynaClient)
+	case "PUT":
+		return handlers.UpdateUser(req, tableName, dynaClient)
+	case "POST":
+		return handlers.DeleteUser(req, tableName, dynaClient)
+	}
+	default: 
+		return handlers.UnhanledReq()
+}
---
+++ Server_4/go.mod
@@ -0,0 +1,3 @@
+module github.com/zarif007/GO-Serverless-CRUD/Server_4
+
+go 1.18
